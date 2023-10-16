7d726b6e95f9716867f031bdf1172952d87658b9
Kirill Korolev <kakorolev@edu.hse.ru>
2023-10-16 13:35:00 +0000

    lm implemented

--- hw_asr/metric/cer_metric.py
+++ hw_asr/metric/cer_metric.py
@@ -28,18 +28,22 @@ class ArgmaxCERMetric(BaseMetric):
 
 
 class BeamSearchCERMetric(BaseMetric):
-    def __init__(self, text_encoder: BaseTextEncoder, beam_size, *args, **kwargs):
+    def __init__(self, text_encoder: BaseTextEncoder, beam_size=3, with_lm=False, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.text_encoder = text_encoder
         self.beam_size = beam_size
-
+        self.with_lm = with_lm
+    
     def __call__(self, log_probs: Tensor, log_probs_length: Tensor, text: List[str], **kwargs):
         cers = []
         predictions = log_probs.detach().cpu()
-        lengths = log_probs_length.detach().cpu().numpy()
+        lengths = log_probs_length.detach().cpu()
+
         for log_prob_vec, length, target_text in zip(predictions, lengths, text):
             target_text = BaseTextEncoder.normalize_text(target_text)
-            if hasattr(self.text_encoder, "ctc_beam_search"):
+            if self.with_lm and hasattr(self.text_encoder, "ctc_lm_beam_search"):
+                pred_text = self.text_encoder.ctc_lm_beam_search(log_prob_vec, length, beam_size=self.beam_size)[0].text
+            elif hasattr(self.text_encoder, "ctc_beam_search"):
                 pred_text = self.text_encoder.ctc_beam_search(log_prob_vec, length, beam_size=self.beam_size)[0].text
             else:
                 pred_text = self.text_encoder.decode(log_prob_vec[:length])
--- hw_asr/text_encoder/ctc_char_text_encoder.py
+++ hw_asr/text_encoder/ctc_char_text_encoder.py
@@ -4,6 +4,8 @@ import torch
 from collections import defaultdict
 
 from .char_text_encoder import CharTextEncoder
+from pyctcdecode import build_ctcdecoder
+import multiprocessing
 
 
 class Hypothesis(NamedTuple):
@@ -14,14 +16,18 @@ class Hypothesis(NamedTuple):
 class CTCCharTextEncoder(CharTextEncoder):
     EMPTY_TOK = "^"
 
-    def __init__(self, alphabet: List[str] = None):
+    def __init__(self, alphabet: List[str] = None, with_lm=False, kenlm_path=None):
         super().__init__(alphabet)
         vocab = [self.EMPTY_TOK] + list(self.alphabet)
         self.ind2char = dict(enumerate(vocab))
         self.char2ind = {v: k for k, v in self.ind2char.items()}
+        self.with_lm = with_lm
+
+        if self.with_lm:
+            assert kenlm_path is not None, "Empty KENLM path"
+            self.decoder = build_ctcdecoder(list(self.alphabet), kenlm_model_path=kenlm_path)
 
     def ctc_decode(self, inds: List[int]) -> str:
-        # TODO: your code here
         last_char = self.EMPTY_TOK
         res = []
         for ind in inds:
@@ -37,56 +43,58 @@ class CTCCharTextEncoder(CharTextEncoder):
     
         return ''.join(res)
 
+    def _extend_beam(self, beam, prob):
+        if len(beam) == 0:
+            for p in prob:
+                last_char = self.ind2char[i]
+                beam[('', last_char)] += p
+            return beam
+
+        new_beam = defaultdict(float)
+        
+        for (prefix, last_char), prefix_proba in beam.items():
+            for i in range(len(prob)):
+                cur_char = self.ind2char[i]
+                if cur_char == last_char:
+                    new_beam[(prefix, last_char)] += prefix_proba * prob[i]
+                else:
+                    new_last_char = cur_char
+                    new_prefix = (prefix + last_char).replace(self.EMPTY_TOK, '')
+                    new_beam[(new_prefix, new_last_char)] += prefix_proba * prob[i]
+        return new_beam
+
+    def _truncate_beam(self, beam, beam_size):
+        return dict(sorted(beam.items(), key=lambda x: -x[1])[:beam_size])
 
     def ctc_beam_search(self, probs, probs_length, beam_size: int = 100) -> List[Hypothesis]:
-        """
-        Performs beam search and returns a list of pairs (hypothesis, hypothesis probability).
-        :param probs: probabilities from model of shape [L, H]
-        :param beam_size: size of beam to use in decoding
-
-        Note: unpadding of probs should be done before passing to this function
-        """
         assert len(probs.shape) == 2
         char_length, voc_size = probs.shape
         assert voc_size == len(self.ind2char)
     
         beam = defaultdict(float)
-
-        probs = torch.softmax(probs[:probs_length], dim=1)
+        probs = probs[:probs_length]
 
         for prob in probs:
             beam = self._extend_beam(beam, prob)
-            beam = self._cut_beam(beam, beam_size)
+            beam = self._truncate_beam(beam, beam_size)
 
         final_beam = defaultdict(float)
-        for (sentence, last_char), v in beam.items():
+        for (sentence, last_char), p in beam.items():
             final_sentence = (sentence + last_char).strip().replace(self.EMPTY_TOK, "")
-            final_beam[final_sentence] += v
+            final_beam[final_sentence] += p
             
         sorted_beam = sorted(final_beam.items(), key=lambda x: -x[1])
-        result = [Hypothesis(sentence, v) \
-                for sentence, v in sorted_beam]
-        return result
+        return [Hypothesis(text, v) for text, v in sorted_beam]
+    
+    def ctc_lm_beam_search(self, probs, probs_length, beam_size: int = 100) -> List[Hypothesis]:
+        assert probs.shape[-1] == len(self.ind2char)
 
-    def _extend_beam(self, beam, prob):
-        if len(beam) == 0:
-            for i in range(len(prob)):
-                last_char = self.ind2char[i]
-                beam[('', last_char)] += prob[i]
-            return beam
+        if len(probs.shape) == 2:
+            probs = probs.unsqueeze(0)
 
-        new_beam = defaultdict(float)
+        probs = [probs[i, :probs_length[i]] for i in range(probs.shape[0])]
+
+        with multiprocessing.get_context("fork").Pool() as pool:
+            texts = self.lm_decoder.decode_batch(pool, probs, beam_width=beam_size)
         
-        for (sentence, last_char), v in beam.items():
-            for i in range(len(prob)):
-                if self.ind2char[i] == last_char:
-                    new_beam[(sentence, last_char)] += v * prob[i]
-                else:
-                    new_last_char = self.ind2char[i]
-                    new_sentence = (sentence + last_char).replace(self.EMPTY_TOK, '')
-                    new_beam[(new_sentence, new_last_char)] += v * prob[i]
-
-        return new_beam
-
-    def _cut_beam(self, beam, beam_size):
-        return dict(sorted(beam.items(), key=lambda x: -x[1])[:beam_size])
+        return texts
