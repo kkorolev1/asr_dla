19591324dc9766680be3a0824af326369d9ca0a7
Kirill Korolev <kakorolev@edu.hse.ru>
2023-10-24 11:08:09 +0000

    old model code sanity

--- hw_asr/model/conformer/feed_forward.py
+++ hw_asr/model/conformer/feed_forward.py
@@ -1,3 +1,4 @@
+'''
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -17,4 +18,29 @@ class FeedForwardModule(nn.Module):
 
     def forward(self, x):
         return self.sequential(x)
+'''
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
+
+class FeedForwardModule(nn.Module):
+    def __init__(self, encoder_dim, feed_forward_expansion=2, dropout=0.1):
+        super().__init__()
+        self.sequential = nn.Sequential(
+            nn.LayerNorm(encoder_dim),
+            nn.Linear(encoder_dim, feed_forward_expansion * encoder_dim),
+            nn.SiLU(),
+            nn.Dropout(dropout),
+            nn.Linear(feed_forward_expansion * encoder_dim, encoder_dim),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        return self.sequential(x)
+    
+
+if __name__ == "__main__":
+    batch = torch.ones((32, 20, 32))
+    ffm = FeedForwardModule(32)
+    print(ffm(batch).shape)
