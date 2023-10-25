2e01dc6816129f4d519c9c5c53f1698d7dd6a03c
Kirill Korolev <kakorolev@edu.hse.ru>
2023-10-25 15:09:05 +0000

    fix scheduler

--- hw_asr/utils/lr_scheduler.py
+++ hw_asr/utils/lr_scheduler.py
@@ -38,6 +38,9 @@ class CosineAnnealingWithWarmupLR(torch.optim.lr_scheduler._LRScheduler):
         self.max_steps = max_steps
         super().__init__(optimizer)
 
+    def get_lr(self):
+        return self.get_last_lr()
+
     def get_last_lr(self):
         lr_factor = self.get_lr_factor(epoch=self.last_epoch)
         return [base_lr * lr_factor for base_lr in self.base_lrs]
